c926933a7407e9f7de7b079530248cd75c92abdb
Kubalo <90120543+KubaloPL@users.noreply.github.com>
2025-11-21 16:26:14 +0100

    Refactor PracownikInfo1 class and fix formatting

--- pracownikinfo1.java
+++ pracownikinfo1.java
@@ -4,27 +4,30 @@ public class PracownikInfo1
 {
     public static void main(String args[])
     {
-    wol[0] = new Pracownik("JK",3000,00,2025,11,14);
-    System.out.print("Nazwa:"+ wol[0].getNazwa()+"Pensja:" + wol[0].getPensja()+"Wyrok: "+ wol[0].getDzat());
+        Pracownik[] wol = new Pracownik[1];
+
+        wol[0] = new Pracownik("JK", 3000.00, 2025, 11, 14);
+
+        System.out.print("Nazwa: " + wol[0].getNazwa() + " Pensja: " + wol[0].getPensja() + " Wyrok: " + wol[0].getDzat());
     }
-    class Pracownik
+
+    static class Pracownik
     {
         private String nazwa;
         private double pensja;
         private Date dzat; //data zatrudnienia
 
         //konstruktor
-
         public Pracownik(String n, double p, int r, int m, int d)
         {
             nazwa = n;
             pensja = p;
-            GregorianCalendar rmd = new GregorianCalendar(r,m-1, d);
+            // m-1 is used because Calendar months are 0-indexed (0 is January)
+            GregorianCalendar rmd = new GregorianCalendar(r, m-1, d);
             dzat = rmd.getTime();
         }
 
-        //metoda (a)
-
+        //metody
         public String getNazwa()
         {
             return nazwa;
@@ -39,6 +42,5 @@ public class PracownikInfo1
         {
             return pensja;
         }
-
     }
 }
